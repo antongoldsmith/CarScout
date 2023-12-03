a8c1ecbc35084322c6c4b1b12c9c5f426ff61bea
Anton Goldsmith <antongoldsmith365@gmail.com>
2023-12-02 19:58:00 -0600

    Initial Commit

---
+++ CarScout/CarScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarScoutApp.swift
+//  CarScout
+//
+//  Created by Anton Goldsmith on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CarScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
